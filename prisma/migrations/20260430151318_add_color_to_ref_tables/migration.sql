5b5d1230809f2804bca2efa0a0be91898bb94211
Nicolas Barron Sour <71356372+nbarsou@users.noreply.github.com>
2026-04-30 09:13:34 -0600

    feat(db): add color column to all ref tables

---
+++ prisma/migrations/20260430151318_add_color_to_ref_tables/migration.sql
@@ -0,0 +1,20 @@
+-- AlterTable
+ALTER TABLE "ref_agreement_type" ADD COLUMN     "color" VARCHAR(7);
+
+-- AlterTable
+ALTER TABLE "ref_attr" ADD COLUMN     "color" VARCHAR(7);
+
+-- AlterTable
+ALTER TABLE "ref_beneficiary" ADD COLUMN     "color" VARCHAR(7);
+
+-- AlterTable
+ALTER TABLE "ref_campus" ADD COLUMN     "color" VARCHAR(7);
+
+-- AlterTable
+ALTER TABLE "ref_giro" ADD COLUMN     "color" VARCHAR(7);
+
+-- AlterTable
+ALTER TABLE "ref_pais" ADD COLUMN     "color" VARCHAR(7);
+
+-- AlterTable
+ALTER TABLE "ref_region" ADD COLUMN     "color" VARCHAR(7);
